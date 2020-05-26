cb72eee6c23e10160c05322f746f50d6574b53ed
ayi <345930286@qq.com>
2020-05-26 09:13:38 +0800

    union model

--- 2005250-unionModel.md
+++ 2005250-unionModel.md
@@ -17,19 +17,19 @@ koa 案例
 const Koa = require("koa");
 const app = new Koa();
 
-app.use(asycn (ctx, next) => {
+app.use(async (ctx, next) => {
     console.log(1);
     await next();
     console.log(2);
 });
 
-app.use(asycn (ctx, next) => {
+app.use(async (ctx, next) => {
     console.log(3);
     await next();
     console.log(4);
 });
 
-app.use(asycn (ctx, next) => {
+app.use(async (ctx, next) => {
     console.log(5);
     await next();
     console.log(6);
--- handwriting/unionModel.js
+++ handwriting/unionModel.js
@@ -2,10 +2,59 @@ class Koa {
     constructor() {
         this.middlewares = []
     }
-    use = function (fn) {
+    use = (fn) => {
+        // 忽略上下文参数 ctx
         this.middlewares.push(fn)
     }
-    compose = function () {
-        
+    compose = () => {
+        const _this = this;
+        return async () => {
+            async function dispatch(i) {
+                if (i === _this.middlewares.length) return null;
+                let fn = _this.middlewares[i];
+                await fn(() => {
+                    dispatch(i + 1)
+                })
+            }
+            await dispatch(0)
+        }
+    }
+    listen = () => {
+        console.log(this.middlewares)
+        let fn = this.compose()
+        fn()
     }
 }
+
+// 异步函数
+function fn() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+            console.log("hello");
+        }, 3000);
+    });
+}
+
+const app = new Koa();
+
+app.use(async (next) => {
+    console.log(1);
+    await next();
+    console.log(2);
+});
+
+app.use(async (next) => {
+    console.log(3);
+    await fn();
+    await next();
+    console.log(4);
+});
+
+app.use(async (next) => {
+    console.log(5);
+    await next();
+    console.log(6);
+});
+
+app.listen(3000);
